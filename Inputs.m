3241bf5f7592b8e83a0d593fcd5e8a4115e7378f
Preetham Manjunatha <preethamam@gmail.com>
2022-04-03 01:11:17 -0700

    Tested on Adobe datasets.

--- Inputs.m
+++ Inputs.m
@@ -26,7 +26,7 @@ input.numCores = 4;            % Number of cores for parallel processing
 
 %% Inputs 3
 % Warping
-input.warpType = 'spherical';   % 'spherical' | 'cylindrical' | 'planar' (projective)
+input.warpType = 'planar';   % 'spherical' | 'cylindrical' | 'planar' (projective)
 input.focalLength = 1200;       % focal length of camera in pixels
 input.k1 = 0;                   % Radial distortion coefficient
 input.k2 = 0;                   % Radial distortion coefficient
@@ -41,7 +41,7 @@ input.Ratiothreshold = 0.6;             % ratio in the range (0,1]
 % Image matching (RANSAC)
 input.Inliersconfidence = 99.9;         % Inlier confidence [0,100]
 input.maxIter = 2000;                   % RANSAC maximum iterations
-input.Transformationtype = 'rigid'; % 'rigid' | 'similarity' | 'affine' | 'projective'
+input.Transformationtype = 'projective'; % 'rigid' | 'similarity' | 'affine' | 'projective'
 input.MaxDistance = 1.50;               % Maximum distance (pixels)
 
 % Image blending and panorama
